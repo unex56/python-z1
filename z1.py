2d064c35e49ee09aeb5a13144ac1572fbc65f4cc
unex56 <slavadenga2@gmail.com>
2026-04-27 09:52:01 +0500

    z1.py

--- z1.py
+++ z1.py
@@ -1,4 +1,10 @@
-vid = input("Вид питомца: ")
-ag = int(input("Возраст питомца: "))
-kl = input("Кличка питомца: ")
-print(f'Это {vid} по кличке "{kl}". Возраст: {ag}.')
+vid = input("Вид питомца:")
+age = int(input("Возраст(в годах):"))
+klich = input("Кличка:")
+if age % 10 == 1 and age % 100 != 11:
+    year_word = "год"
+elif 2 <= age % 10 <= 4 and (age % 100 < 10 or age % 100 >= 20):
+    year_word = "года"
+else:
+    year_word = "лет"
+print(f'Это {vid} по кличке "{klich}". Возраст: {age} {year_word}')
